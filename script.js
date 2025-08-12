5a1a6c6de5219b6896b827620bd073152b76ea1a
Michael Quelazar G <quelazar23@gmail.com>
2025-08-12 21:34:02 +0530

    initial files

---
+++ script.js
@@ -0,0 +1,14 @@
+// Responsive navigation toggle
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+if (navToggle && navLinks) {
+  navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
+  // Close nav on link click (mobile)
+  navLinks.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      navLinks.classList.remove('open');
+    });
+  });
+} 
